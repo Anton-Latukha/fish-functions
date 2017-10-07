09ad39e09c0b47867bb9f8f5abf214505279d6ee
Anton-Latukha <anton.latukha@gmail.com>
2017-10-07 21:35:50 +0300

    prompt: sps

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -19,9 +19,9 @@ function fish_prompt
   set -l normal_color     (set_color normal)
   set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
   set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
-  
+
   set -l directory_color
-  
+
   if test $USER = 'root'
     set directory_color  (set_color $fish_color_error ^/dev/null; or set_color red)
   else
