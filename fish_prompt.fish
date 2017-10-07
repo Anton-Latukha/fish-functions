4e09dbca9e829db60768828de69d0a24540472a7
Anton-Latukha <anton.latukha@gmail.com>
2017-10-07 21:25:19 +0300

    prompt: mark root user with red prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
 
-	set -l last_command_status $status
+  set -l last_command_status $status
   set -l cwd
 
   if test "$theme_short_path" = 'yes'
@@ -19,7 +19,15 @@ function fish_prompt
   set -l normal_color     (set_color normal)
   set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
   set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
-  set -l directory_color  (set_color $fish_color_cwd ^/dev/null; or set_color green)
+  
+  set -l directory_color
+  
+  if test $USER = 'root'
+    set directory_color  (set_color $fish_color_error ^/dev/null; or set_color red)
+  else
+    set directory_color  (set_color $fish_color_cwd ^/dev/null; or set_color green)
+  end
+
   set -l repository_color (set_color $fish_color_quote ^/dev/null; or set_color brown)
 
   if git_is_repo
@@ -27,11 +35,10 @@ function fish_prompt
       set root_folder (command git rev-parse --show-toplevel ^/dev/null)
       set parent_root_folder (dirname $root_folder)
       set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-
-      echo -n -s $directory_color $cwd $normal_color
-    else
       echo -n -s $directory_color $cwd $normal_color
     end
+
+    echo -n -s $directory_color $cwd $normal_color
 
     echo -n -s (set_color brgreen) ':' $repository_color (git_branch_name) $normal_color
 
