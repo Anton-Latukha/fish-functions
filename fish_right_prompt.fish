c89270868744245733b926a0f54110326dcc0565
Anton-Latukha <anton.latukha@gmail.com>
2017-10-07 20:55:29 +0300

    add right_prompt

---
+++ fish_right_prompt.fish
@@ -0,0 +1,11 @@
+function fish_right_prompt --description 'Right part of prompt'
+    
+    # Save our status
+    set -l last_comand_status $status
+
+    set -l last_status_string ""
+    if [ $last_comand_status -ne 0 ]
+        printf "%s(%d)%s" (set_color red --bold) $last_comand_status (set_color normal)
+    end
+    
+end
