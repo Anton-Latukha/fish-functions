6a3f57cfac19cfd325bd3c4a5b6a6dcb91d2749f
Anton-Latukha <anton.latukha@gmail.com>
2019-01-17 09:57:43 +0200

    upd nrsu

--- nrsu.fish
+++ nrsu.fish
@@ -1,6 +1,6 @@
 function nrsu
 
-    s fish -c 'cd /etc/nixos ; git add . ; git commit -m "upd conf" ; git push'
+    s fish -c 'cd /etc/nixos && git add . && git commit -m "upd conf" && git push && git push hub'
     s -i nix-channel --update unstable
     s -i nixos-rebuild switch --upgrade $argv
 
