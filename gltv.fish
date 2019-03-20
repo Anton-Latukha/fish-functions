5b4972ea5b12e9225eee4954cfe3c434b66f1592
Anton-Latukha <anton.latukha@gmail.com>
2019-03-20 13:47:47 +0200

    add gltv: git log --graph (AKA tree) (verbose)

---
+++ gltv.fish
@@ -0,0 +1,8 @@
+function gltv
+
+  # V is for Verbose
+  # Branch tree of the current branch
+  # From: https://stackoverflow.com/questions/1838873/visualizing-branch-topology-in-git/37041706
+  git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'
+
+end
