3240e92fd1aee9212172865d82dcc0ef4bc67216
Anton-Latukha <anton.latukha@gmail.com>
2019-03-28 01:10:11 +0200

    upd crHasPr

--- crHasPr.fish
+++ crHasPr.fish
@@ -1,7 +1,12 @@
 function crHasPr
 
-  set haskellProjectTemplate '/home/pyro/src/haskell/Haskell-Programming-From-First-Principles/projectTemplate/'
+  set -l haskellProjectTemplate '/home/pyro/src/haskell/Haskell-Programming-From-First-Principles/projectTemplate/'
+  set -l projName $argv
+  set -l projNameCabal (echo $argv | tr -d '.')
 
   cp -r $haskellProjectTemplate $argv
+  cd $projName
+  # Change first line to name of the project
+  sed -i -e "1s/.*/name:                  $projNameCabal/" .'/main.cabal'
 
 end
