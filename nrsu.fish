4e277f78eb65cf588a5c32fe3f4fc9f4bbd56cdc
Anton-Latukha <anton.latukha@gmail.com>
2019-04-05 23:50:52 +0300

    upd nrsu: mv remotes

--- nrsu.fish
+++ nrsu.fish
@@ -1,6 +1,6 @@
 function nrsu
 
-  sudo fish -c 'cd /etc/nixos && git add . && git commit -m "upd conf" && git push l && git push h'
+  sudo fish -c 'cd /etc/nixos && git add . && git commit -m "upd conf" && git push u && git push h'
   sudo -i nix-channel --update unstable
   sudo  -i nixos-rebuild switch --upgrade $argv
 
