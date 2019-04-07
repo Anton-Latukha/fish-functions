1a874051d6dfdba5d3e74a7e0cfd2d6aa25818e6
Anton-Latukha <anton.latukha@gmail.com>
2019-04-07 10:55:19 +0300

    2019-04-07: autocommit

---
+++ find-autocommit-repos.fish
@@ -0,0 +1,8 @@
+function find-autocommit-repos
+
+  # argv[1] - directory to search under
+
+  # Find dirs containinf `.autocommit` file, and get their directory
+  dirname (fd -H '\.autocommit' $argv[1] -x readlink -f) | sort -u
+
+end
