269c9fd7453ef654f2c5cf8f8db3aeedd45d4278
Anton-Latukha <anton.latukha@gmail.com>
2019-04-08 13:06:37 +0300

    upd mr-add-commit-push

--- mr-add-commmit-push.fish
+++ mr-add-commmit-push.fish
@@ -2,9 +2,9 @@ function mr-add-commmit-push
 
   set -l date (date --iso)
   set -l notes ~/org
-  set -l repos $notes $notes/journal $notes/mantra
+  set -l repos $notes/journal $notes/mantra $notes
   for repo in $repos
-    mr -n -d $repo commit -m $date
+    mr -n -d $repo commit -m $date': autocommit'
   end
 
 end
