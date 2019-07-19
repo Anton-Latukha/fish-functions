800bda842a3fb36442e9e051547d7fd7c5c82640
Anton-Latukha <anton.latukha@gmail.com>
2019-07-19 23:28:04 +0300

    upd site

--- site.fish
+++ site.fish
@@ -1,5 +1,14 @@
 function site
 
-  command sh ~/.config/fish/functions/site.sh $argv
+  cd /home/pyro/src/mkdocs-blog/mkdocs-material
+  git checkout master
+  git pull u master
+  git push f master
+  git checkout custom
+  git rebase master
+  docker build -t mkdocs-material .
+  cd /home/pyro/src/mkdocs-blog
+  docker run --rm -it -p 8000:8000 -v (pwd):/docs mkdocs-material build
+  docker run --rm -it -p 8000:8000 -v (pwd):/docs mkdocs-material
 
 end
