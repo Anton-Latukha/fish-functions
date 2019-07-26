2a0f825d0847ef8786c889ae29e5dd6ce1b8e6de
Anton-Latukha <anton.latukha@gmail.com>
2019-07-26 20:43:28 +0300

    upd site: upd haskell-notes

--- site.fish
+++ site.fish
@@ -1,15 +1,20 @@
 function site
 
-  cd /home/pyro/src/mkdocs-blog/mkdocs-material
+  set haskellNotes "$HOME"/org/haskell
+  set mkdocsBlog "$HOME"/src/mkdocs-blog
+
+  cd "$mkdocsBlog"/mkdocs-material
   git checkout master
   git pull u master
   git push f master
   git checkout custom
   git rebase master
   docker build -t mkdocs-material .
-  cd /home/pyro/src/mkdocs-blog
-  rm /home/pyro/src/mkdocs-blog/docs/haskell.html
-  cp /home/pyro/org/haskell/README.html /home/pyro/src/mkdocs-blog/docs/haskell-notes.html
+  cd "$mkdocsBlog"
+  rm "$mkdocsBlog"/docs/haskell.html
+  cp -r "$haskellNotes"/Book/ "$mkdocsBlog"/docs/
+  cp -r "$haskellNotes"/Good_code/ "$mkdocsBlog"/docs/
+  cp "$haskellNotes"/README.html "$mkdocsBlog"/docs/haskell-notes.html
   docker run --rm -it -p 8000:8000 -v (pwd):/docs mkdocs-material build
   docker run --rm -it -p 8000:8000 -v (pwd):/docs mkdocs-material
 
