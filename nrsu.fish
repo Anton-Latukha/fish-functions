179dc5e2f7858aec26337bffa3d5f89cb83368cb
Anton-Latukha <anton.latukha@gmail.com>
2019-10-20 03:06:56 +0300

    upd nrsu

--- nrsu.fish
+++ nrsu.fish
@@ -1,7 +1,12 @@
 function nrsu
 
-  sudo fish -c 'set -l SSH_AUTH_SOCK /run/user/1000/keepass-ssh-agent.socket ; cd /etc/nixos && git add . && git commit -m "upd conf" && git push u'
-  sudo -i nix-channel --update unstable
-  sudo  -i nixos-rebuild switch --upgrade $argv
+  sudo fish -c '\
+    set -l SSH_AUTH_SOCK /run/user/1000/keepass-ssh-agent.socket ; \
+    cd /etc/nixos && \
+    git add . && \
+    git commit -m "upd conf" \
+    && git push u'
+  # sudo -i nix-channel --update unstable
+  sudo -i nixos-rebuild switch --upgrade $argv
 
 end
