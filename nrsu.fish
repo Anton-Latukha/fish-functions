4e7480701856a3b1599860c6f4160867d7f1daf8
Anton-Latukha <anton.latukha@gmail.com>
2019-10-20 03:28:30 +0300

    upd nrsu

--- nrsu.fish
+++ nrsu.fish
@@ -3,6 +3,7 @@ function nrsu
   sudo fish -c '\
     set SSH_AUTH_SOCK /run/user/1000/keepass-ssh-agent.socket ; \
     cd /etc/nixos && \
+    git pull && \
     git add . && \
     git commit -m "upd conf" ; \
     git push u'
