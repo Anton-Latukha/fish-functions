304b8f1d6bfac80c829e83cc32950564c19d45c6
Anton-Latukha <anton.latukha@gmail.com>
2020-02-03 20:21:17 +0200

    nghcid: upd

--- nghcid.fish
+++ nghcid.fish
@@ -1,5 +1,8 @@
-function nghcid
+function nghcid $argv
 
-  nix-shell --command "ghcid --command='cabal v2-repl --repl-options=-fno-code --repl-options=-fno-break-on-exception --repl-options=-fno-break-on-error --repl-options=-v1 --repl-options=-ferror-spans --repl-options=-j' "
+  ##  2019-02-03: NOTE: Doing:
+  # nix-shell --command 'ghcid --command="cabal v2-repl --repl-options=-fno-code --repl-options=-fno-break-on-exception --repl-options=-fno-break-on-error --repl-options=-v1 --repl-options=-ferror-spans --repl-options=-j" '
+  # Launching fish function for GHCID
+  nix-shell --command "fish -c 'gid'"
 
 end
