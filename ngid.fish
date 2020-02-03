13f8be5b368af3f01f520a4f35a65ae352f96e98
Anton-Latukha <anton.latukha@gmail.com>
2020-02-03 20:21:48 +0200

    ng(hc -> )id

--- ngid.fish
+++ ngid.fish
@@ -1,4 +1,4 @@
-function nghcid $argv
+function ngid $argv
 
   ##  2019-02-03: NOTE: Doing:
   # nix-shell --command 'ghcid --command="cabal v2-repl --repl-options=-fno-code --repl-options=-fno-break-on-exception --repl-options=-fno-break-on-error --repl-options=-v1 --repl-options=-ferror-spans --repl-options=-j" '
