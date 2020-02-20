7433a569940ee2d3f29a99f3957b63cf98753cd9
Anton-Latukha <anton.latukha@gmail.com>
2020-02-20 19:50:17 +0200

    add unBorkBogusNextcloudSync

---
+++ unBorkBogusNextcloudSync.fish
@@ -0,0 +1,12 @@
+function unBorkBogusNextcloudSync
+
+  for file in (fd -H '\(conflicted copy.*\)')
+    set -l cleanFileName (printf "$file" | sed 's/ (conflicted copy .*)//g')
+    set -l protectFilesFromNextCloud (printf '%s.protectThisFromNextCloud' "$cleanFileName")
+    printf 'Moving: %s -> %s\n' "$cleanFileName" "$protectFilesFromNextCloud"
+    mv "$cleanFileName" "$protectFilesFromNextCloud"
+    printf 'Moving: %s -> %s\n' "$file" "$cleanFileName"
+    mv "$file" "$cleanFileName"
+  end
+
+end
