451ea7c5767f4f5a2ad1702374a31b58ba3296ca
Anton-Latukha <anton.latukha@gmail.com>
2020-03-06 12:21:54 +0200

    pskill: upd

--- pskill.fish
+++ pskill.fish
@@ -2,15 +2,18 @@ function pskill
 
 # Soft kill the process by its name
 # Send signals with increasing strictness and damage to the process
-set posixSignal 1 15 2 9
 
-for sign in $posixSignal
+  set posixSignal 1 15 2 9
 
-pkill -0 $argv
-if test $status -eq 0
-  echo "Sending -$sign SIGN to process $argv"
-  pkill -1 $argv
-  sleep 15
-end
+  for sign in $posixSignal
+
+    pkill -0 $argv
+    if test $status -eq 0
+      echo "Sending -$sign SIGN to process $argv"
+      pkill -1 $argv
+      sleep 15
+    end
+
+  end
 
 end
