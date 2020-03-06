018f040ea1addaa03a797c0c526f1ca605ca43d0
Anton-Latukha <anton.latukha@gmail.com>
2020-03-07 00:49:56 +0200

    add fzf wrapper

---
+++ fzf.fish
@@ -0,0 +1,6 @@
+function fzf
+## Wrapper for fzf, to configure it nicely
+
+command fzf --ansi --layout=reverse --info=hidden --height=20% --no-bold --color='fg:#ffffff,fg+:#ffffff,bg:#000000,bg+:#393939,gutter:#000000,hl:#ffaa00,hl+:#ffaa00,preview-fg:#ffffff,preview-bg:#000000,border:#000000,pointer:#000000'
+
+end
