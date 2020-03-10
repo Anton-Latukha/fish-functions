1938e200fb3dd440f233f1d5bdeb5b8661cb8560
Anton-Latukha <anton.latukha@gmail.com>
2020-03-10 18:09:32 +0200

    book-prepare: upd

--- book-prepare.fish
+++ book-prepare.fish
@@ -1,9 +1,9 @@
-function book-publish
+function book-prepare
 
   set haskellNotes "$HOME"/org/haskell
   set bookFilenameTemplate "$haskellNotes"/README
   set book "$bookFilenameTemplate".org
 
-  dd if=/dev/null of="$book" bs=1 seek=$(printf $(stat --format=%s "$book" ) - $(tail -n1 "$book" | wc -c) | bc )
+  dd if=/dev/null of="$book" bs=1 seek=(math (stat --format=%s "$book" ) - (tail -n1 "$book" | wc -c) | bc)
 
 end
