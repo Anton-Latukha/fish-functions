5816371945317064a7b2a57be5b8dbab8e54cd9b
Anton-Latukha <anton.latukha@gmail.com>
2020-03-14 18:21:30 +0200

    book-publish: upd
    M  book-prepare.fish

--- book-prepare.fish
+++ book-prepare.fish
@@ -5,6 +5,6 @@ function book-prepare
   set book "$bookFilenameTemplate".org
 
   # Calculate the size of the last line in chars and remove that size of bytes from the end of the file; in-place erases last line
-  dd if=/dev/null of="$book" bs=1 seek=(math (stat --format=%s "$book") - (tail -n1 "$book" | wc -c))
+  dd if=/dev/null of="$book" bs=1 seek=(math (stat --format=%s "$book") - (tail -n2 "$book" | wc -c))
 
 end
