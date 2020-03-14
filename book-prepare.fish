52b9734dcd9dba8de0fbb7a1e97618f76a7fc0c7
Anton-Latukha <anton.latukha@gmail.com>
2020-03-14 19:08:19 +0200

    book-prepare: upd
    M  book-prepare.fish

--- book-prepare.fish
+++ book-prepare.fish
@@ -4,7 +4,9 @@ function book-prepare
   set bookFilenameTemplate "$haskellNotes"/README
   set book "$bookFilenameTemplate".org
 
-  # Calculate the size of the last line in chars and remove that size of bytes from the end of the file; in-place erases last line
-  dd if=/dev/null of="$book" bs=1 seek=(math (stat --format=%s "$book") - (tail -n2 "$book" | wc -c))
+  # # Calculate the size of the last line in chars and remove that size of bytes from the end of the file; in-place erases last line
+  # dd if=/dev/null of="$book" bs=1 seek=(math (stat --format=%s "$book") - (tail -n2 "$book" | wc -c))
+
+  sed -i '/^#\+latex_header: \\pagestyle\{empty\}$/d' ./haskell.org
 
 end
