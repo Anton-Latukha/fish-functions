964ffbf5348e251b45711adb17901fce7eedcd47
Anton-Latukha <anton.latukha@gmail.com>
2020-03-24 00:28:50 +0200

    book-finish: upd
    M  book-finish.fish

--- book-finish.fish
+++ book-finish.fish
@@ -4,6 +4,13 @@ function book-finish
   set bookFilenameTemplate "$haskellNotes"/README
   set book "$bookFilenameTemplate".org
 
+  ## HACK for Org-mode LaTeX preview
+
+  sed -i -e '/^#+latex_header: \\\usepackage{unicode-math}.*$/d' \
+         -e '/^#+latex_header: \\\unimathsetup{math-style=.*$/d' \
+         -e '/^#+latex_header: \\\usepackage\[a-1b\]{pdfx}.*$/d' \
+            "$bookFilenameTemplate".org
+
   ## Reintroducing the HACK erased by book-prepare
   echo '#+latex_header: \pagestyle{empty}' >> "$book"
 
