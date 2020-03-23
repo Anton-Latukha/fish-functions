77f7e9914017d65b4c6f78e993d5dd058e4b828c
Anton-Latukha <anton.latukha@gmail.com>
2020-03-24 00:23:12 +0200

    book-prepare: upd
    M  book-prepare.fish

--- book-prepare.fish
+++ book-prepare.fish
@@ -7,7 +7,7 @@ function book-prepare
   # # Calculate the size of the last line in chars and remove that size of bytes from the end of the file; in-place erases last line
   # dd if=/dev/null of="$book" bs=1 seek=(math (stat --format=%s "$book") - (tail -n2 "$book" | wc -c))
 
-  sed -i -e '/^#+latex_header: \\\pagestyle{empty}$/d' \
+  sed -i -e '/^#+latex_header: \\\pagestyle{empty}.*$/d' \
          -e '/^#+latex_header: \\\usepackage{unicode-math}.*$/d' \
          -e '/^#+latex_header: \\\unimathsetup{math-style=.*$/d' \
          -e '/^#+latex_header: \\\usepackage\[a-1b\]{pdfx}.*$/d' \
