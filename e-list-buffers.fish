a316b6563195ac284325431d70d2e2eb64679b65
Anton-Latukha <anton.latukha@gmail.com>
2020-04-03 20:34:34 +0300

    e-list-buffers: upd

--- e-list-buffers.fish
+++ e-list-buffers.fish
@@ -1,5 +1,6 @@
 function e-list-buffers
 
+  # Output the buffer list to stdout, then remove prelude and arbitrary number of spaces, then trailing '>'
   e -n -e "(princ (buffer-list) #'external-debugging-output)" | sed -E 's/#<buffer[[:space:]]+/\n/g' | tr '>' ' '
 
 end
