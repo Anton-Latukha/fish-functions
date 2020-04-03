2697c84ed495cfdb5bfc059b86714a4109b79fd9
Anton-Latukha <anton.latukha@gmail.com>
2020-04-03 20:33:10 +0300

    e-list-buffers: upd
    M  e-kill-buffer.fish

--- e-kill-buffer.fish
+++ e-kill-buffer.fish
@@ -1,5 +1,6 @@
 function e-kill-buffer
 
+  # Save buffer noninteractively (without confirmation), then kill the buffer
   e --eval '(progn (save-some-buffers "'"$argv"'" t) (kill-buffer "'"$argv"'"))'
 
 end
