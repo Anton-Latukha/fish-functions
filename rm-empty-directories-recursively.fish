ee887d63b92310f3fb3d1f31db5a4226c012b63b
Anton-Latukha <anton.latukha@gmail.com>
2020-05-03 21:59:57 +0300

    upd rm-empty-directories-recursively
    M  rm-empty-directories-recursively.fish

--- rm-empty-directories-recursively.fish
+++ rm-empty-directories-recursively.fish
@@ -2,7 +2,7 @@ function rm-empty-directories-recursively
 
   # Delete all empty directories
   set dirList (get-empty-directories $argv)
-  while test -z $dirList
+  while test -n $dirList
     rm -d $dirList
     set dirList (get-empty-directories $argv)
   end
