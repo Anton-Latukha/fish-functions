42c51d8e942e839ffec3fd630026960ce5446988
Anton-Latukha <anton.latukha@gmail.com>
2020-05-03 21:48:31 +0300

    upd rm-empty-directories
    M  rm-empty-directories.fish

--- rm-empty-directories.fish
+++ rm-empty-directories.fish
@@ -1,6 +1,6 @@
 function rm-empty-directories
 
   # Delete all empty files that are directories
-  fd --type d --type e --absolute-path $argv --exec-batch rmdir {}
+  rmdir (get-empty-directories $argv)
 
 end
