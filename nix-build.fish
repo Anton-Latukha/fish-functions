2fe2f28021d5e276dab0a3c81341423cbfff0ff2
Anton-Latukha <anton.latukha@gmail.com>
2020-05-18 04:11:03 +0300

    nix-build: upd
    M  nix-build.fish

--- nix-build.fish
+++ nix-build.fish
@@ -2,14 +2,19 @@ function nix-build
 
 ## Lazify the output of the nix-build.
 
+# If build successful - return last `$numOfLines`
+# else - print the log into the terminal
+
   set -l tmpFile (mktemp)
   echo "Log file: $tmpFile"
 
+  set -l numOfLines 1
+
   if command nix-build $argv &> "$tmpFile"
     echo "Build succesfull!"
-    tail $tmpFile
+    tail -n"$numOfLines" "$tmpFile"
   else
-    cat $tmpFile
+    cat "$tmpFile"
   end
 
 end
