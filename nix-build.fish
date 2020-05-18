5328501ae089557dd10371c249214cf3e91c1cd0
Anton-Latukha <anton.latukha@gmail.com>
2020-05-18 04:05:22 +0300

    nix-build: upd
    M  nix-build.fish

--- nix-build.fish
+++ nix-build.fish
@@ -1,6 +1,10 @@
 function nix-build
 
+## Lazify the output of the nix-build.
+
   set -l tmpFile (mktemp)
+  echo "Log file: $tmpFile"
+
   if command nix-build $argv &> "$tmpFile"
     echo "Build succesfull!"
     tail $tmpFile
