32d2f7fd0074aabbd64f463df889f4356a8aed44
Anton-Latukha <anton.latukha@gmail.com>
2020-05-19 13:41:16 +0300

    book-prepare: upd
    M  book-prepare.fish

--- book-prepare.fish
+++ book-prepare.fish
@@ -10,7 +10,7 @@ function book-prepare
   sed -i -e '/^#+latex_header: \\\pagestyle{empty}.*$/d' \
             "$book"
   echo '#+latex_header: \usepackage{unicode-math}    % Typesetting engine for math fonts. Docs: http://mirror.datacenter.by/pub/mirrors/CTAN/macros/latex/contrib/unicode-math/unicode-math.pdf
-  #+latex_header: \unimathsetup{math-style=ISO}    % Configures the unicode-math. ISO italizes all letters, bold also
-  #+latex_header: \usepackage[a-1b]{pdfx}   % Produce the PDF to according ISO standard. Can has issues with LuaTeX: http://mirror.datacenter.by/pub/mirrors/CTAN/macros/latex/contrib/pdfx/pdfx.pdf note 2 Dev recommends to use it right after documentclass, but since it uses iflatex, I think this is a good idea to keep it here.' >> "$book"
+#+latex_header: \unimathsetup{math-style=ISO}    % Configures the unicode-math. ISO italizes all letters, bold also
+#+latex_header: \usepackage[a-1b]{pdfx}   % Produce the PDF to according ISO standard. Can has issues with LuaTeX: http://mirror.datacenter.by/pub/mirrors/CTAN/macros/latex/contrib/pdfx/pdfx.pdf note 2 Dev recommends to use it right after documentclass, but since it uses iflatex, I think this is a good idea to keep it here.' >> "$book"
 
 end
