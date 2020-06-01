bd2384ebac347a9047391c0aae728e5b9d97031b
Anton-Latukha <anton.latukha@gmail.com>
2020-06-02 01:41:55 +0300

    ngid: upd
    M  ngid.fish

--- ngid.fish
+++ ngid.fish
@@ -3,6 +3,6 @@ function ngid
   ##  2019-02-03: NOTE: Doing:
   # nix-shell --command 'ghcid --command="cabal v2-repl --repl-options=-fno-code --repl-options=-fno-break-on-exception --repl-options=-fno-break-on-error --repl-options=-v1 --repl-options=-ferror-spans --repl-options=-j" '
   # Launching fish function for GHCID
-  nix-shell --command "fish -c 'gid'" $argv
+  nix-shell --command "fish -c 'gid' $argv"
 
 end
