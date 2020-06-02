f0f91a712f8a6cb26163d1b3a10bfae47c9a12aa
Anton-Latukha <anton.latukha@gmail.com>
2020-06-02 03:23:23 +0300

    gid: upd
    M  gid.fish

--- gid.fish
+++ gid.fish
@@ -1,5 +1,5 @@
 function gid
 
-  ghcid --command='cabal v2-repl --repl-options=-fno-code --repl-options=-fno-break-on-exception --repl-options=-fno-break-on-error --repl-options=-v1 --repl-options=-ferror-spans --repl-options=-j'
+  ghcid --command='cabal v2-repl --repl-options=-fno-code --repl-options=-fno-break-on-exception --repl-options=-fno-break-on-error --repl-options=-v1 --repl-options=-ferror-spans --repl-options=-j  --repl-options=-Wunused-imports'
 
 end
