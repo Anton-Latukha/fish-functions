6804a281699c7e3ad8b4593f23f079d892ac4c57
Anton-Latukha <anton.latukha@gmail.com>
2020-06-12 00:06:20 +0300

    M  git-find-add-commit-push-after-unchanged-N-timeout.fish

--- git-find-add-commit-push-after-unchanged-N-timeout.fish
+++ git-find-add-commit-push-after-unchanged-N-timeout.fish
@@ -1,11 +1,13 @@
 function git-find-add-commit-push-after-unchanged-N-timeout
 
   # argv[1] - timeout in `fd` terms
+  set -l timeout $argv[1]
   # argv[2] - message of commit
+  set -l message $argv[2]
 
   # Get a list of files, concatenete lines into one string and pass the string to git add
-  git add (git-find-unchanged-uncommited-in-N-timeout $argv[1] | string join ' ')
-  git commit --untracked-files=no --message $argv[2]
+  git add (git-find-unchanged-uncommited-in-N-timeout "$timeout" | string join ' ')
+  git commit --untracked-files=no --message "$message"
   git push
 
 end
