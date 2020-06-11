461a975d4d1862b380f920b67c24de4a07ca8d8a
Anton-Latukha <anton.latukha@gmail.com>
2020-06-12 00:16:34 +0300

    M  find-autocommit-repos.fish

--- find-autocommit-repos.fish
+++ find-autocommit-repos.fish
@@ -3,6 +3,6 @@ function find-autocommit-repos
   # argv[1] - directory to search under
 
   # Find dirs containinf `.autocommit` file, and get their directory
-  dirname (fd -H '\.autocommit' $argv[1] -x readlink -f) | sort -u
+  dirname (command fd -H '\.autocommit' $argv[1] -x readlink -f) | sort -u
 
 end
