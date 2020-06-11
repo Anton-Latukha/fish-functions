62a74e2a5dce7cad8354f40a7950a89160924c61
Anton-Latukha <anton.latukha@gmail.com>
2020-06-12 01:35:36 +0300

    M  git-list-untracked-files.fish

--- git-list-untracked-files.fish
+++ git-list-untracked-files.fish
@@ -1,5 +1,10 @@
 function git-list-untracked-files
 
+  set -e git
+  set -e awk
+
+  type git
+
   git status --untracked-files=all --ignore-submodules=dirty --short | awk '{print $2}' | sort -u
 
 end
