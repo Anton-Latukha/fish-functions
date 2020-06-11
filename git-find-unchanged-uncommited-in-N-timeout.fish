162df3eb28cccb07e0878c6b97aba4c2f3969b85
Anton-Latukha <anton.latukha@gmail.com>
2020-06-12 00:07:19 +0300

    M  git-find-unchanged-uncommited-in-N-timeout.fish

--- git-find-unchanged-uncommited-in-N-timeout.fish
+++ git-find-unchanged-uncommited-in-N-timeout.fish
@@ -1,8 +1,10 @@
 function git-find-unchanged-uncommited-in-N-timeout
 
   # argv[1] is a timeout in `fd` terms
+  set -l timeout $argv[1]
+
   comm -12 \
     (git-list-untracked-files | psub) \
-    (find-unchanged-in-N-timeout $argv[1] | psub)
+    (find-unchanged-in-N-timeout "$timeout" | psub)
 
 end
