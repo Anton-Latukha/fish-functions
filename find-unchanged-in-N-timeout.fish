8779e5b1e006ee113da7351146c40dfb09ae5b7d
Anton-Latukha <anton.latukha@gmail.com>
2020-06-12 00:18:00 +0300

    M  find-unchanged-in-N-timeout.fish

--- find-unchanged-in-N-timeout.fish
+++ find-unchanged-in-N-timeout.fish
@@ -2,6 +2,6 @@ function find-unchanged-in-N-timeout
 
   set -l timeout $argv[1]
 
-  fd -H --change-older-than "$timeout" -E'.git' . | sort -u
+  command fd -H --change-older-than "$timeout" -E'.git' . | command sort -u
 
 end
