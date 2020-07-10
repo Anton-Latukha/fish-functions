71644d1fab3df7ea9cee9a233320f1011876398b
Anton-Latukha <anton.latukha@gmail.com>
2020-07-10 20:24:26 +0300

    M  git-pass-filter.fish

--- git-pass-filter.fish
+++ git-pass-filter.fish
@@ -5,15 +5,15 @@ set passFilter ''
 # Form a `-v pattern` for `rg`
 for pattern in $argv
 
-  set passFilter = "$passFilter -v $pattern"
+  set passFilter = "$passFilter|$pattern"
 
 end
 
 
 git filter-branch --prune-empty --index-filter \
-                               'git ls-tree -z -r --name-only --full-tree $GIT_COMMIT | \
-                               rg --null-data '"$pattern"' | \
-                               xargs -0 -r git rm --cached -r' -- --all
+  'git ls-tree -z -r --name-only --full-tree $GIT_COMMIT | \
+  rg --null-data -v \''"$pattern"'\' | \
+  xargs -0 -r git rm --cached -r' -- --all
 
 
 end
