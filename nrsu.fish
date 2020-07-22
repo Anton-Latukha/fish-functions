7118277c51345342ce148b8526d0457616ce5f83
Anton-Latukha <anton.latukha@gmail.com>
2020-07-22 14:20:23 +0300

    M  nrsu.fish

--- nrsu.fish
+++ nrsu.fish
@@ -7,7 +7,7 @@ function nrsu
     git add . && \
     git commit -m "upd conf" ; \
     git push u'
-  # sudo -i nix-channel --update unstable
+  sudo -i nix-channel --update
   sudo -i nixos-rebuild switch --upgrade $argv
 
 end
