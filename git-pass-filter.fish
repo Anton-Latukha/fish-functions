59aadd036189b2fefcec8533e1ff2a933444e572
Anton-Latukha <anton.latukha@gmail.com>
2020-08-12 15:07:47 +0300

    git-pass-filter.fish: rm garbage collector

--- git-pass-filter.fish
+++ git-pass-filter.fish
@@ -21,7 +21,4 @@ git filter-branch --prune-empty --index-filter \
   rg --null-data -v \''"$passFilter"'\' | \
   xargs -0 -r git rm --cached -r' -- --all
 
-# Aggresive garbage collect
-git gc --aggressive
-
 end
