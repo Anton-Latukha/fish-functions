76b9f7e9a3f4614cac2fd09e167674a27c433963
Anton-Latukha <anton.latukha@gmail.com>
2020-10-20 18:52:45 +0300

    D  gh.fish in favour of GitHub CLI

--- gh.fish
+++
@@ -1,9 +0,0 @@
-function gh
-
-  if command -q -v bat
-    bat ~/.config/git/config | sed -e '1,/\[alias\]/d' -e '/#/d' -e 's/^[ \t]*//'
-  else
-    cat ~/.config/git/config | sed -e '1,/\[alias\]/d' -e '/#/d' -e 's/^[ \t]*//'
-  end
-
-end
