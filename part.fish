e7bcd0a1dd8e46be94bb0bcda230305d52de5e92
Anton-Latukha <anton.latukha@gmail.com>
2020-11-11 19:44:39 +0200

    M  part.fish

--- part.fish
+++ part.fish
@@ -1,6 +1,6 @@
 function part
 
   man $argv[1] | awk 'BEGIN {RS=""}/^[[:space:]]*'$argv[2]'/{print $0}'
-  man -P '/run/current-system/sw/bin/less' $argv[1] | awk 'BEGIN {RS=""}/^[[:space:]]*'$argv[2]'/{print $0}'
+  man -P 'less' $argv[1] | awk 'BEGIN {RS=""}/^[[:space:]]*'$argv[2]'/{print $0}'
 
 end
