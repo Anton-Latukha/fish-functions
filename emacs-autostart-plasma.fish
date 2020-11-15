228608dbfe1c1c547a42ed248bd5d0d8f857eaab
Anton-Latukha <anton.latukha@gmail.com>
2020-11-15 20:45:42 +0200

    D  emacs-autostart-plasma.fish

--- emacs-autostart-plasma.fish
+++
@@ -1,7 +0,0 @@
-function emacs-autostart-plasma
-
-  ## Requires user systemd service & wmctrl
-  systemctl --user import-environment PATH S O H HH HB NP N XDG_RUNTIME_DIR SSH_AUTH_SOCK
-  systemctl --user start emacs && sleep 8 && command emacsclient -c & && sleep 2 && wmctrl -r 'emacs@rogue' -t 0
-
-end
