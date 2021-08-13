43030bb3379005e9392ca585d88f50f9d219f306
Anton-Latukha <anton.latukha@gmail.com>
2021-08-13 16:43:28 +0300

    M  hshowlang.fish

--- hshowlang.fish
+++ hshowlang.fish
@@ -1,5 +1,6 @@
 function hshowlang
 
+  # Get the language pragmas, sort & count their uses and print their use frequency. 
   hrg '# language' --no-heading --no-filename --no-messages --no-line-number | sort | uniq -c | sort -n -r
 
 end
