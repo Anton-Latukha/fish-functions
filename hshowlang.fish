2af74c32e995eae5854d3bd51f96de032a91bf29
Anton-Latukha <anton.latukha@gmail.com>
2021-08-13 16:54:53 +0300

    M  hshowlang.fish

--- hshowlang.fish
+++ hshowlang.fish
@@ -1,6 +1,6 @@
 function hshowlang
 
   # Get the language pragmas, sort & count their uses and print their use frequency.
-  hrg '# language' --no-heading --no-filename --no-messages --no-line-number | sed 's/...$//' | awk '{$1=$1;print}' | sort | uniq -c | sort -n -r
+  hrg -i '# language' --no-heading --no-filename --no-messages --no-line-number | sed -e 's/^....language//I' -e 's/...$//' | awk '{$1=$1;print}' | sort | uniq -c | sort -n -r
 
 end
